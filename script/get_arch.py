aa246a68fe75b4443722f87e63722629bc71c9c3
Breanne Crockett <51859778+bcrocke3@users.noreply.github.com>
2024-10-22 15:22:45 -0600

    Move get_arch.py from Py2 to Py3

--- script/get_arch.py
+++ script/get_arch.py
@@ -1,15 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # This script maps the output of `gcc -dumpmachine` to supported
 # platforms of alchemy (TARGET_ARCH variable).
 # `gcc -dumpmachine` is more reliable than `uname -a` inside docker containers
 # Refer to the CMakeLists.txt file for more information.
 
-from __future__ import print_function
 import subprocess
 
 try:
-  arch = subprocess.check_output(["gcc", "-dumpmachine"]).strip().split("-")[0]
+  arch = subprocess.check_output(["gcc", "-dumpmachine"]).decode('utf-8').strip().split("-")[0]
   if arch in ["aarch64", "arm64"]:
     o = "aarch64"
   elif arch in ["x86_64", "amd64"]:
